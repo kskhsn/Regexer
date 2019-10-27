020e29e7f591d3b1f9b5882a1f00cbce9634d07c
KeisukeHoshino <kskhsn15@gmail.com>
2019-10-27 17:48:51 +0900

    タイトルの修正

--- Regexer/ViewModels/MainWindowViewModel.cs
+++ Regexer/ViewModels/MainWindowViewModel.cs
@@ -5,7 +5,7 @@ namespace Regexer.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Regexer";
         public string Title
         {
             get { return _title; }
